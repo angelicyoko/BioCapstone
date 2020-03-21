64daf6c41d0a91701b5e93711528636e0dcd063d
Manisha Kusuma <manishak@hornbeam.rlogin>
2020-03-21 14:22:24 -0400

    Added strength graph initial

--- collectData.py
+++ collectData.py
@@ -1,5 +1,7 @@
 import csv
 import json
+import matplotlib.pyplot as plt
+import networkx as nx
 
 ######################### READ DATA #########################
 data = []
@@ -24,13 +26,13 @@ with open('Dataset2.csv', encoding='utf-8', mode = 'r') as csvFile:
 ### For example: data = [('MOB_one', 'MOB_one', 0), ('MOB_one', 'MOB_two', 0)...]
 ### tuple[0] is the start node
 ### tuple[1] is the target node
-### tuple[3] is the weight (has already been casted to int)
+### tuple[2] is the weight (has already been casted to int)
 
 ### Jack's TODO ###
 graph = nx.MultiDiGraph()
 for line in data:
     if (line[2] != ""):
-        graph.add_edge(line[0], line[1], line[2])
+        graph.add_edge(line[0], line[1], weight=line[2])
 # Uncomment this to show a basic plot of the graph
 # nx.draw(graph)
 # plt.show()
@@ -42,3 +44,60 @@ for line in data:
 
 ####################### STENGTH GRAPH #######################
 ### Manisha's TODO ###
+
+#Dictionary objects holding strength values ie. { 'MOD' : 25, ... }
+strengthIN = {}
+strengthOUT = {}
+for n in graph.nodes(): #Iterate through nodes in the graph
+	node = n.split("_")[0]
+	hemisphere = n.split("_")[1]
+	sumIn = 0
+	sumOut = 0
+	#Iterate through all the in edges of a node
+	#Calculate the total in strength 
+	#Only consider edges in hemisphere ie (one->one, left->left, right->right)
+	for u,v,w in graph.in_edges(n, data=True):
+		hem  = v.split("_")[1]
+		if hem == "one" and hemisphere == "one":
+			sumIn += w.get("weight")
+		elif hem == "left" and hemisphere == "left":
+                        sumIn += w.get("weight")
+		elif hem == "right" and hemisphere == "right":
+                        sumIn += w.get("weight")
+	#Iterate through all the out edges of a node
+        #Calculate the total out strength
+        #Only consider edges in hemisphere (one->one, left->left, right->right)
+	for u,v,w in graph.out_edges(n, data=True):
+                hem  = v.split("_")[1]
+                if hem == "one" and hemisphere == "one":
+                        sumOut += w.get("weight")
+                elif hem == "left" and hemisphere == "left":
+                        sumOut += w.get("weight")
+                elif hem == "right" and hemisphere == "right":
+                        sumOut += w.get("weight")
+	#Making sure there are no repeat nodes in the dictonary
+	#MOD_one and MOD_left = MOD in the dictonary
+	if node in strengthIN:
+		strengthIN[node] = strengthIN.get(node) + sumIn
+	else:	
+		strengthIN[node] = sumIn
+	if node in strengthOUT:
+		strengthOUT[node] = strengthOUT.get(node) + sumOut
+	else:
+		strengthOUT[node] = sumOut
+
+#Input Strength Graph
+plt.figure(figsize=(40,10))
+plt.bar(strengthIN.keys(), strengthIN.values())
+plt.savefig('inStrength.png')
+plt.xlabel('Nodes')
+plt.ylabel('Strength')
+
+#Output Strength
+plt.figure(figsize=(40,10))
+plt.bar(strengthOUT.keys(), strengthOUT.values())
+plt.savefig('outStrength.png')
+plt.xlabel('Nodes')
+plt.ylabel('Strength')
+
+
