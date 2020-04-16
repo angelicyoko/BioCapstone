e63cbd2cfafaa71220e42c9027b027de820fc160
Manisha Kusuma <manishak@mulberry.rlogin>
2020-04-16 11:11:34 -0400

    fixed refactoring conflict with graphs

--- collectData.py
+++ collectData.py
@@ -4,28 +4,37 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 ######################### READ DATA #########################
-from graph import degree_help
+from graph import graph_help
 
 data = []
+test = []
 with open('Dataset2.csv', encoding='utf-8', mode='r') as csvFile:
-    read = csv.reader(csvFile)
-    # start to record when begin = 3
-    begin = 0
-    for line in read:
-        if begin >= 3:
-            sub = {}
-            sub["start"] = str(line[1]) + "_" + str(line[0])
-            sub["target"] = str(line[3]) + "_" + str(line[2])
-            sub["data_type"] = str(line[4]) if len(line[4]) != 0 else ""
-            sub["binned_strength"] = int(line[5]) if len(line[5]) != 0 else ""
-
-            tuple_sub = (sub["start"], sub["target"], sub["binned_strength"])
-            data.append(tuple_sub)
-        begin += 1
+	read = csv.reader(csvFile)
+	begin = 0
+	count = 0
+	for line in read:
+		if begin >= 3:
+			sub = {}
+			sub["start"] = str(line[1])
+			sub["target"] = str(line[3])
+			if str(line[0]) == str(line[2]):
+				count += 1
+				if sub['start'] == "" or sub['target'] == '':
+					continue
+				sub["binned_strength"] = int(line[5]) if len(line[5]) != 0 else ""
+				if sub["binned_strength"] == '':
+					break
+				if sub["binned_strength"] == 0:
+					continue
+				tuple_sub_weight = (sub["start"], sub["target"], sub["binned_strength"])
+				tuple_sub = (sub["start"], sub["target"])
+				if tuple_sub not in test:
+					test.append(tuple_sub)
+					data.append(tuple_sub_weight)
+		begin += 1
 
 ################# STORE DATA INTO NETWORKX ##################
 ### you will need variable "data" which will be a list of a bunch of tuples
-### For example: data = [('MOB_one', 'MOB_one', 0), ('MOB_one', 'MOB_two', 0)...]
 ### tuple[0] is the start node
 ### tuple[1] is the target node
 ### tuple[2] is the weight (has already been casted to int)
@@ -33,89 +42,10 @@ graph = nx.MultiDiGraph()
 for line in data:
     if (line[2] != ""):
         graph.add_edge(line[0], line[1], weight=line[2])
-# Uncomment this to show a basic plot of the graph
-# nx.draw(graph)
-# plt.show()
 
-# List of 77 nodes that are included in the Degree and Strength Graphs Dataset
-seventy_seven_nodes = ["ECT", "VISp", "VISal", "TEa", "VISpm", "SSs", "VISam", "VISrl", "SSp", "AUDv", "6b", "MOp",
-                       "VISlm", "AUDp", "PTLp", "AUDpo", "VISpl", "AUDd", "VISli", "VISll", "VISlla", "ENTl", "ORBm",
-                       "PERI", "PIR", "LA", "AIp", "PL", "BLAp", "ENTm", "EPd", "ILA", "BLAa", "COApm", "AIv", "CA1v",
-                       "VISC", "AId", "AOA", "TR", "GU", "EPv", "PAA", "NLOT", "BMAp", "SUBv", "BMAa", "CA3", "COApl",
-                       "COAa", "NLOT3", "CA1d", "TTd", "SUBd", "PA", "MOB", "TTv", "DG", "CA2", "IG", "AOB", "FC",
-                       "MOs", "RSPd", "CLA", "ACAv", "ORBv", "RSPv.a", "ACAd", "ORBvl", "RSPv.b/c", "PAR", "POST",
-                       "RSPagl", "PRE", "ORBl", "RSPv", "ECT", "VISp", "VISal", "TEa", "VISpm", "SSs", "VISam", "VISrl",
-                       "SSp", "AUDv", "6b", "MOp", "VISlm", "AUDp", "PTLp", "AUDpo", "VISpl", "AUDd", "VISli", "VISll",
-                       "VISlla", "ENTl", "ORBm", "PERI", "PIR", "LA", "AIp", "PL", "BLAp", "ENTm", "EPd", "ILA", "BLAa",
-                       "COApm", "AIv", "CA1v", "VISC", "AId", "AOA", "TR", "GU", "EPv", "PAA", "NLOT", "BMAp", "SUBv",
-                       "BMAa", "CA3", "COApl", "COAa", "NLOT3", "CA1d", "TTd", "SUBd", "PA", "MOB", "TTv", "DG", "CA2",
-                       "IG", "AOB", "FC", "MOs", "CLA", "RSPd", "ACAv", "ORBv", "ACAd", "RSPv.a", "RSPv.b/c", "POST",
-                       "RSPagl", "PAR", "PRE", "ORBl", "ORBvl", "RSPv"]
+####################### DEGREE AND STRENGTH GRAPH ########################
+graph_help(graph)
 
-####################### DEGREE GRAPH ########################
-degree_help()
-
-####################### STENGTH GRAPH #######################
-# Dictionary objects holding strength values ie. { 'MOD' : 25, ... }
-strengthIN = {}
-strengthOUT = {}
-for n in graph.nodes():  # Iterate through nodes in the graph
-    node = n.split("_")[0]
-    if node in seventy_seven_nodes:
-        hemisphere = n.split("_")[1]
-        sumIn = 0
-        sumOut = 0
-        # Iterate through all the in edges of a node
-        # Calculate the total in strength
-        # Only consider edges in hemisphere ie (one->one, left->left, right->right)
-        for u, v, w in graph.in_edges(n, data=True):
-            name = v.split("_")[0]
-            if name in seventy_seven_nodes:
-                hem = v.split("_")[1]
-                if hem == "one" and hemisphere == "one":
-                    sumIn += w.get("weight")
-                elif hem == "left" and hemisphere == "left":
-                    sumIn += w.get("weight")
-                elif hem == "right" and hemisphere == "right":
-                    sumIn += w.get("weight")
-        # Iterate through all the out edges of a node
-        # Calculate the total out strength
-        # Only consider edges in hemisphere (one->one, left->left, right->right)
-        for u, v, w in graph.out_edges(n, data=True):
-            name = v.split("_")[0]
-            if name in seventy_seven_nodes:
-                hem = v.split("_")[1]
-                if hem == "one" and hemisphere == "one":
-                    sumOut += w.get("weight")
-                elif hem == "left" and hemisphere == "left":
-                    sumOut += w.get("weight")
-                elif hem == "right" and hemisphere == "right":
-                    sumOut += w.get("weight")
-        # Making sure there are no repeat nodes in the dictonary
-        # MOD_one and MOD_left = MOD in the dictonary
-        if node in strengthIN:
-            strengthIN[node] = strengthIN.get(node) + sumIn
-        else:
-            strengthIN[node] = sumIn
-        if node in strengthOUT:
-            strengthOUT[node] = strengthOUT.get(node) + sumOut
-        else:
-            strengthOUT[node] = sumOut
-
-# print(len(strengthOUT.keys()))
-# Input Strength Graph
-plt.figure(figsize=(40, 10))
-plt.bar(strengthIN.keys(), strengthIN.values())
-plt.savefig('inStrength.png')
-plt.xlabel('Nodes')
-plt.ylabel('Strength')
-
-# Output Strength
-plt.figure(figsize=(40, 10))
-plt.bar(strengthOUT.keys(), strengthOUT.values())
-plt.savefig('outStrength.png')
-plt.xlabel('Nodes')
-plt.ylabel('Strength')
 
 ####################### Rich Club Analysis #######################
 # TODO: Ashley
--- graph.py
+++ graph.py
@@ -5,80 +5,7 @@ import matplotlib.pyplot as plt
 
 ######################### READ DATA #########################
 
-def degree_help():
-	data = []
-	test = []
-	with open('Dataset2.csv', encoding='utf-8', mode='r') as csvFile:
-		read = csv.reader(csvFile)
-		# start to record when begin = 3
-		begin = 0
-		count = 0
-		for line in read:
-			if begin >= 3:
-				sub = {}
-				sub["start"] = str(line[1])
-				sub["target"] = str(line[3])
-				if str(line[0]) == str(line[2]):
-					count += 1
-					if sub['start'] == "" or sub['target'] == '':
-						continue
-					sub["binned_strength"] = int(line[5]) if len(line[5]) != 0 else ""
-					if sub["binned_strength"] == '':
-						break
-					if sub["binned_strength"] == 0:
-						continue
-					tuple_sub_weight = (sub["start"], sub["target"], sub["binned_strength"])
-					tuple_sub = (sub["start"], sub["target"])
-					if tuple_sub not in test:
-						#	find = [i for i, v in enumerate(data) if v[0] == sub["start"] and v[1] == sub["target"]]
-						#	find = find[0]
-						#	y = list(data[find])
-						#	y[2] += sub["binned_strength"]
-						#	data[find] = tuple(y)
-						# else:
-						test.append(tuple_sub)
-						data.append(tuple_sub_weight)
-			begin += 1
-		#print("count is: " + str(count))
-
-	## To Json ##
-
-	# with open('final.json', 'w', encoding='utf-8') as f:
-	#     json.dump(data, f, indent = 4)
-
-	## read data ##
-	'''
-	f = open('out.json',)
-	tuples = json.load(f)
-	data = []
-	for tupl in tuples: 
-	    t = (tupl[0], tupl[1], tupl[2])
-		data.append(t)
-	f.close()
-	'''
-
-	# f = open('out.json',)
-	# data = json.load(f)
-	# f.close()
-
-	################# STORE DATA INTO NETWORKX ##################
-	### you will need variable "data" which will be a list of a bunch of tuples
-	### For example: data = [('MOB_one', 'MOB_one', 0), ('MOB_one', 'MOB_two', 0)...]
-	### tuple[0] is the start node
-	### tuple[1] is the target node
-	### tuple[3] is the weight (has already been casted to int)
-
-	### Jack's TODO ###
-	graph = nx.MultiDiGraph()
-	for line in data:
-		graph.add_edge(line[0], line[1], weight=line[2])
-	# print("number of nodes: " + str(len(G.nodes())))
-	# print("number of edges: " + str(G.number_of_edges()))
-	# print("out degree: " + str(G.out_degree('FC')))
-	# print("in degree: " + str(G.in_degree('FC')))
-	# Uncomment this to show a basic plot of the graph
-	# nx.draw(graph)
-	# plt.show()
+def graph_help(graph):
 
 	####################### DEGREE GRAPH ########################
 	### Ashley's TODO ###
@@ -122,69 +49,68 @@ def degree_help():
 	plt.ylabel('degree')
 
 
-# ####################### STENGTH GRAPH #######################
-# ### Manisha's TODO ###
-# #Input Strength Graph
-#
-# strengthIN = {}
-# strengthOUT = {}
-# for n in graph.nodes():
-# 	if n in nodesI:
-# 		sumIn = 0
-# 		sumOut = 0
-# 		for u,v,w in graph.in_edges(n, data=True):
-# 			if v in nodesI:
-# 				if(w.get("weight") == 7):
-# 					sumOut += 10**0;
-# 				elif(w.get("weight") == 6):
-#                                          sumOut += 10**-0.333;
-# 				elif(w.get("weight") == 5):
-#                                          sumOut += 10**-0.66;
-# 				elif(w.get("weight") == 4):
-#                                          sumOut += 10**-1;
-# 				elif(w.get("weight") == 3):
-#                                          sumOut += 10**-2;
-# 				elif(w.get("weight") == 2):
-#                                          sumOut += 10**-3;
-# 				elif(w.get("weight") == 1):
-#                                          sumOut += 10**-4;
-# 				else:
-#                                          sumOut += 0;
-# 		#print(graph.out_edges(n, data=True))
-# 		for u,v,w in graph.out_edges(n, data=True):
-# 			if v in nodesI:
-# 				if(w.get("weight") == 7):
-#                                          sumIn += 10**0;
-# 				elif(w.get("weight") == 6):
-#                                          sumIn += 10**-0.333;
-# 				elif(w.get("weight") == 5):
-#                                          sumIn += 10**-0.66;
-# 				elif(w.get("weight") == 4):
-#                                          sumIn += 10**-1;
-# 				elif(w.get("weight") == 3):
-#                                          sumIn += 10**-2;
-# 				elif(w.get("weight") == 2):
-#                                          sumIn += 10**-3;
-# 				elif(w.get("weight") == 1):
-#                                          sumIn += 10**-4;
-# 				else:
-#                                          sumIn += 0;
-# 		strengthIN[n] = sumIn
-# 		strengthOUT[n] = sumOut
-#
-# sorted_inD = {k: v for k, v in sorted(strengthIN.items(), key=lambda item: item[1])}
-# sorted_outD = {k: v for k, v in sorted(strengthOUT.items(), key=lambda item: item[1])}
-#
-# #Input Strength Graph
-# plt.figure(figsize=(40,10))
-# plt.bar(sorted_inD.keys(), sorted_inD.values())
-# plt.savefig('inStrength.png')
-# plt.xlabel('Nodes')
-# plt.ylabel('Strength')
-#
-# #Output Strength
-# plt.figure(figsize=(40,10))
-# plt.bar(sorted_outD.keys(), sorted_outD.values())
-# plt.savefig('outStrength.png')
-# plt.xlabel('Nodes')
-# plt.ylabel('Strength')
+	####################### STENGTH GRAPH #######################
+	### Manisha's TODO ###
+	#Input Strength Graph
+
+	strengthIN = {}
+	strengthOUT = {}
+	for n in graph.nodes():
+		if n in nodesI:
+			sumIn = 0
+			sumOut = 0
+			for u,v,w in graph.in_edges(n, data=True):
+				if v in nodesI:
+					if(w.get("weight") == 7):
+						sumOut += 10**0;
+					elif(w.get("weight") == 6):
+        	                                sumOut += 10**-0.333;
+					elif(w.get("weight") == 5):
+                        	                sumOut += 10**-0.66;
+					elif(w.get("weight") == 4):
+                                        	sumOut += 10**-1;
+					elif(w.get("weight") == 3):
+        	                                sumOut += 10**-2;
+					elif(w.get("weight") == 2):
+                        	                sumOut += 10**-3;
+					elif(w.get("weight") == 1):
+						sumOut += 10**-4;
+				else:
+                                         sumOut += 0;
+			for u,v,w in graph.out_edges(n, data=True):
+				if v in nodesI:
+					if(w.get("weight") == 7):
+                                        	 sumIn += 10**0;
+					elif(w.get("weight") == 6):
+        	                                 sumIn += 10**-0.333;
+					elif(w.get("weight") == 5):
+                        	                 sumIn += 10**-0.66;
+					elif(w.get("weight") == 4):
+                                        	 sumIn += 10**-1;
+					elif(w.get("weight") == 3):
+        	                                 sumIn += 10**-2;
+					elif(w.get("weight") == 2):
+                        	                 sumIn += 10**-3;
+					elif(w.get("weight") == 1):
+                                        	 sumIn += 10**-4;
+					else:
+        	                                 sumIn += 0;
+			strengthIN[n] = sumIn
+			strengthOUT[n] = sumOut
+
+	sorted_inD = {k: v for k, v in sorted(strengthIN.items(), key=lambda item: item[1])}
+	sorted_outD = {k: v for k, v in sorted(strengthOUT.items(), key=lambda item: item[1])}
+
+	#Input Strength Graph
+	plt.figure(figsize=(40,10))
+	plt.bar(sorted_inD.keys(), sorted_inD.values())
+	plt.savefig('inStrength.png')
+	plt.xlabel('Nodes')
+	plt.ylabel('Strength')
+
+	#Output Strength
+	plt.figure(figsize=(40,10))
+	plt.bar(sorted_outD.keys(), sorted_outD.values())
+	plt.savefig('outStrength.png')
+	plt.xlabel('Nodes')
+	plt.ylabel('Strength')
